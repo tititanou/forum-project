b79f6bc047f2324c4a8adfac6a514379b712ed0a
SAID <smatnaziha@yahoo.fr>
2021-12-15 22:51:16 +0100

    Les classes: Message ,Admin,Moderateur et User

--- forum/src/main/java/forum/models/Sujet.java
+++ forum/src/main/java/forum/models/Sujet.java
@@ -8,4 +8,14 @@ public class Sujet {
 	private String libelle;
 	private Date dateCreation;
 	private Long auteurId;
+	@Override
+	public String toString() {
+		return "Sujet [id=" + id + ", libelle=" + libelle + ", dateCreation=" + dateCreation + ", auteurId=" + auteurId
+				+ "]";
+	}
+	public Sujet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 }
